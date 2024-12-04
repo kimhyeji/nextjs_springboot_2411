c510227b94f5faaa999a21a058b0100b69cb2b72
kimhyeji <gpwl5589@gmail.com>
2024-12-04 10:10:06 +0900

    add jwt provider

---
+++ src/main/java/com/rest/global/jwt/JwtProvider.java
@@ -0,0 +1,61 @@
+package com.rest.global.jwt;
+
+import com.rest.global.util.Util;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.crypto.SecretKey;
+import java.util.Base64;
+import java.util.Date;
+import java.util.Map;
+
+public class JwtProvider {
+    @Value("${custom.jwt.secretKey}")
+    private String secretKeyOrigin;
+
+    private SecretKey cachedSecretKey;
+
+    public SecretKey getSecretKey() {
+        if ( cachedSecretKey == null ) cachedSecretKey = _getSecretKey();
+        return cachedSecretKey;
+    }
+    public SecretKey _getSecretKey() {
+        String keyBase64Encoded = Base64.getEncoder().encodeToString(secretKeyOrigin.getBytes());
+        return Keys.hmacShaKeyFor(keyBase64Encoded.getBytes());
+    }
+
+    public String getToken(Map<String, Object> claims, int seconds) {
+        long now = new Date().getTime();
+
+        Date accessTokenExpiresIn  = new Date(now + 1000L * seconds);
+        return Jwts.builder()
+                .claim("body", Util.json.toStr(claims))
+                .setExpiration(accessTokenExpiresIn)
+                .signWith(getSecretKey(), SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+    public boolean verify(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(getSecretKey())
+                    .build()
+                    .parseClaimsJws(token);
+        } catch (Exception e)  {
+            return false;
+        }
+        return true;
+    }
+
+    public Map<String, Object> getClaims(String token) {
+        String body = Jwts.parserBuilder()
+                .setSigningKey(getSecretKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .get("body", String.class);
+        return Util.toMap(body);
+    }
+}
