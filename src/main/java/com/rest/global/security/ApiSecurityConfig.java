bc8008b792796365c3295ea83234c2b8ed740083
kimhyeji <gpwl5589@gmail.com>
2024-12-04 10:29:52 +0900

    add jwt filter

--- src/main/java/com/rest/global/security/ApiSecurityConfig.java
+++ src/main/java/com/rest/global/security/ApiSecurityConfig.java
@@ -4,14 +4,20 @@ package com.rest.global.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class ApiSecurityConfig {
+    private final JwtAuthorizationFilter jwtAuthorizationFilter;
+
     @Bean
     SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
         http
@@ -20,12 +26,27 @@ public class ApiSecurityConfig {
                         authorizeRequests -> authorizeRequests
                                 .requestMatchers("/api/*/articles").permitAll()
                                 .requestMatchers("/api/*/articles/*").permitAll()
+                                .requestMatchers(HttpMethod.POST,"/api/*/members/login").permitAll()
                                 .anyRequest().authenticated()
                 )
                 .csrf(
                         csrf -> csrf
                                 .disable()
-                );
+                ) // csrf 토큰 끄기
+                .httpBasic(
+                        httpBasic -> httpBasic.disable()
+                ) // httpBasic 로그인 방식 끄기
+                .formLogin(
+                        formLogin -> formLogin.disable()
+                ) // 폼 로그인 방식 끄기
+                .sessionManagement(
+                        sessionManagement -> sessionManagement.sessionCreationPolicy(STATELESS)
+                ) // 세션 끄기
+                .addFilterBefore(
+                        jwtAuthorizationFilter, // 액세스 토큰을 이용한 로그인 처리
+                        UsernamePasswordAuthenticationFilter.class
+                )
+        ;
         return http.build();
     }
 }
